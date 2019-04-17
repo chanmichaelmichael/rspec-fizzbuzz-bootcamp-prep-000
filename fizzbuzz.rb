3ed4b2beb54f1e29d2697e7fc59cf91ba7d2a63a
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 07:36:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
-  number = number.to_i
-  if number%3 == 0 && number%5 == 0
+def fizzbuzz(num)
+  num = num.to_i
+  if num%3 == 0 && num%5 == 0
     return "FizzBuzz"
-  elseif number%3 == 0
+  elseif num%3 == 0
     return "Fizz"
-  elseif number%5 == 0
+  elseif num%5 == 0
     return "Buzz"
   end
 end

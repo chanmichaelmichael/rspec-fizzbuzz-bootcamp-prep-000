f2e9b0ab90bd47595519a3735bb6968e3670b74d
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 07:46:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int%3==0 && int%5==0
+  if int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
     return "FizzBuzz"
-  elseif int%3==0
+  elseif int % 3 == 0
     puts "Fizz"
     return "Fizz"
-  elseif int%5==0
+  elseif int % 5 == 0
     puts "Buzz"
     return "Buzz"
   else

b65eb6eab6e0748c6f1e3bf93dd92c3b8767caca
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 07:33:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@
 def fizzbuzz(number)
 #  binding.pry
   if number%3 == 0 && number%5 == 10
-    return"FizzBuzz"
+    return "FizzBuzz"
   elseif number%3 == 0
     return "Fizz"
   elseif number%5 == 0

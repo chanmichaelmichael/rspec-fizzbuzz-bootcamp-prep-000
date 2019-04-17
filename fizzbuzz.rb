22d3e1db32b2dee63a4cd1e45952f1f972bedeaf
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 07:43:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,6 @@
 def fizzbuzz(num)
   if num%3==0 && num%5==0
     puts "FizzBuzz"
-    return "FizzBuzz"
-  elseif num%3==0
-    puts "Fizz"
-    return "Fizz"
-  elseif num%5==0
-    puts "Buzz"
-    return "Buzz"
-  else
-    puts "strange"
-  end
 end
 
 def fizzbuzz2(num)

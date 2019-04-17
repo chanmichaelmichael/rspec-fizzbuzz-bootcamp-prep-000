63b665dd3bae3ef8917b6c761e7e70e760345d37
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 07:26:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+# require 'pry'
+
+def fizzbuzz(number)
+#  binding.pry
+output = nil
+  if (number%3 == 0 && number%5 == 10)
+    output =  "FizzBuzz"
+  elseif number%3 == 0
+    output = "Fizz"
+  elseif number%5 == 0
+    output = "Buzz"
+  end
+  return output
+  
+end

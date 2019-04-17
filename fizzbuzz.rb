501df42a38a6177229fdee7c883c170449859d68
Michael Michael <michael.ta.chan@gmail.com>
2019-04-17 07:38:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,11 @@ def fizzbuzz(num)
   num = num.to_i
   if num%3 == 0 && num%5 == 0
     return "FizzBuzz"
-  elseif num%3 == 0
-    return "Fizz"
-  elseif num%5 == 0
-    return "Buzz"
+  else
+    if num%3 == 0
+      return "Fizz"
+    elseif num%5 == 0
+      return "Buzz"
+    end
   end
 end
